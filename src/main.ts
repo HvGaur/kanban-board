7b0fb95e7450d55067859974b0903baf58dd4f37
HvGaur <hvgaurr@gmail.com>
2025-03-27 01:29:35 +0530

    dev commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { KanbanComponent } from './app/kanban/kanban.component';
-// Create a root container dynamically
+
 const root = document.createElement('app-kanban');
 document.body.appendChild(root);
 
-// Bootstrap the application
 bootstrapApplication(KanbanComponent).catch(err => console.error(err));
